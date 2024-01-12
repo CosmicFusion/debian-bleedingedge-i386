cf01565b466f3f0a662a885b55f5d9fa3572a150
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-12 15:56:25 +0000

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 set -e
 # Run at 07/08/2023 19:40 UTC 3
-DIST=mantic ARCH=i386 debootstrap --arch=i386 mantic base_chroot || true
+DIST=sid ARCH=i386 debootstrap --arch=i386 sid base_chroot || true
 rm -rf base_chroot/debootstrap
-echo 'deb [trusted=yes] https://ppa.pika-os.com/ pikauwu main' > base_chroot/etc/apt/sources.list.d/pika-apt.list
+apt install wget -y
+wget https://github.com/CosmicFusion/debian-bleedingedge/raw/main/setup.sh -O base_chroot/setup.sh
+chmod +x base_chroot/setup.sh
+chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive /setup.sh"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y install sudo git bc gpg gpg-agent wget -y"
-chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/pikauwu/pika-sources.deb"
-chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install ./pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew""
-rm -rf base_chroot/etc/apt/sources.list.d/pika-apt.list
-chroot ./base_chroot /bin/bash -c "rm -rf ./pika-sources.deb"
-chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
-chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y upgrade --allow-downgrades"
-chroot ./base_chroot /bin/bash -c "ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime"
-chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata"
-chroot ./base_chroot /bin/bash -c "apt-get clean"
+chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt full-upgrade -y"
+rm -rf base_chroot/setup.sh
 cd  ./base_chroot
 sudo tar -czvf ../base_chroot.tgz ./*
 
